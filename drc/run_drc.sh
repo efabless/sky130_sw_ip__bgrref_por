42b90f2bea90f18cd9472c87dda4459dde182a76
azwefabless <andrew.wright@efabless.com>
2024-06-16 18:17:03 -0400

    adding klayout and drc run scriptss for drc
    Signed-off-by: azwefabless <andrew.wright@efabless.com>

--- drc/run_drc.sh
+++ drc/run_drc.sh
@@ -1,16 +1,18 @@
 #! /bin/bash
-magic -dnull -noconsole -rcfile /usr/share/pdk/sky130A/libs.tech/magic/sky130A.magicrc << EOF
-load ../mag/sky130_sw_ip__bgrref_por
+project=sky130_sw_ip__bgrref_por
+magic -dnull -noconsole -rcfile ${PDK_ROOT}/${PDK}/libs.tech/magic/sky130A.magicrc << EOF
+load ../mag/${project}
 select top cell
 expand
 drc style drc(full)
 drc on
 drc check
 drc catchup
-drc count
+drc count total
 set tk_version 8.5
 source \${CAD_ROOT}/magic/tcl/drcmgr.tcl
 #feedback save drc.log
-magic::drc_save_report sky130_sw_ip__bgrref_por drc.log
+magic::drc_save_report ${project}  drc.log
 EOF
 exit 0
+
