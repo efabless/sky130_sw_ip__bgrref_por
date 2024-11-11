a7d6fb21fb3a9533a91ea105060b0351839be97c
Tim Edwards <tim@opencircuitdesign.com>
2024-11-11 16:23:53 -0500

    Modified layout to match changes in the schematic design for robust behavior:  (1) Alternating caps in the short delay line switched from vss to vccl to increase the delay; (2) penultimate inverter in the buffered output of the comparator switched from avdd to dvdd; (3) resistor chain generating the bias current multiplied up by 4x to keep the steady-state current well under 1uA to better meet the chip spec for low-power operation.

--- xschem/run_extract_sch.sh
+++ xschem/run_extract_sch.sh
@@ -6,3 +6,5 @@ echo ${PDK_ROOT:=/usr/share/pdk} > /dev/null
 echo ${PDK:=sky130A} > /dev/null
 
 xschem -n -s -r -x -q --tcl "set lvs_netlist 1" --rcfile $PDK_ROOT/$PDK/libs.tech/xschem/xschemrc -o ../netlist/schematic -N ${project}.spice ${project}.sch
+
+echo "Done!"
